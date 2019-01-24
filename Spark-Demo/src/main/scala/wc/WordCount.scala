76f45e8e7345d351db969854f86e7ea24a534cb1
Vaibhav <vaibhavdave5@ccs.neu.edu>
2019-01-24 12:06:40 -0500

    Spark Ready

--- Spark-Demo/src/main/scala/wc/WordCount.scala
+++ Spark-Demo/src/main/scala/wc/WordCount.scala
@@ -23,7 +23,7 @@ object WordCountMain {
 		// ================
     
     val textFile = sc.textFile(args(0))
-    val counts = textFile.flatMap(line => line.split(" "))
+    val counts = textFile.map(line => line.split(",")(0))
                  .map(word => (word, 1))
                  .reduceByKey(_ + _)
     counts.saveAsTextFile(args(1))
