64ac6a54e47714b954694d89a38a3f4703d01f31
Vaibhav <vaibhavdave5@ccs.neu.edu>
2019-02-08 01:46:17 -0500

    TESTED with small output

--- SocialTraingle/MR-Demo/src/main/java/wc/TriangleCount.java
+++ SocialTraingle/MR-Demo/src/main/java/wc/TriangleCount.java
@@ -9,26 +9,18 @@ import org.apache.hadoop.util.Tool;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.hadoop.util.ToolRunner;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.ArrayWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 
 public class TriangleCount extends Configured implements Tool {
@@ -39,8 +31,6 @@ public class TriangleCount extends Configured implements Tool {
 		@Override
 		public void map(final Object key, final Text value, final Context context)
 				throws IOException, InterruptedException {
-			final StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
 
 				// Processing the CSV
 				String[] ids = value.toString().split(",");
@@ -50,11 +40,11 @@ public class TriangleCount extends Configured implements Tool {
 				 * journey the triangle also has its to and from trail
 				 */
 
-				Text from = new Text(ids[0] + ";" + ids[1] + ";" + EdgeDirection.From.toString());
-				Text to = new Text(ids[0] + ";" + ids[1] + ";" + EdgeDirection.To.toString());
+				Text from = new Text(ids[0] + "-" + ids[1] + "-" + EdgeDirection.From.toString());
+				Text to = new Text(ids[0] + "-" + ids[1] + "-" + EdgeDirection.To.toString());
 				context.write(new Text(ids[0]), from);
 				context.write(new Text(ids[1]), to);
-			}
+			
 		}
 	}
 
@@ -68,27 +58,95 @@ public class TriangleCount extends Configured implements Tool {
 			ArrayList<Text> to = new ArrayList<>(); // List of to
 
 			for (Text edge : values) {
-				String[] arr = edge.toString().split(";");
+				String[] arr = edge.toString().split("-");
 				String edgeDirection = arr[2];
 
-				if (edgeDirection.equals("from"))
-					from.add(new Text(arr[0] + ";" + arr[1]));
-				if (edgeDirection.equals("to"))
-					to.add(new Text(arr[0] + ";" + arr[1]));
+				if (edgeDirection.equals(EdgeDirection.From.toString()))
+					from.add(new Text(arr[0] + "-" + arr[1]));
+				if (edgeDirection.equals(EdgeDirection.To.toString()))
+					to.add(new Text(arr[0] + "-" + arr[1]));
 			}
 
 			for (Text fromEdge : from) {
 				for (Text toEdge : to) {
 					// from is a->b and toEdge is b->c
 					// So we want to emit a->c
-					System.out.println(fromEdge.toString()+" "+toEdge.toString());
 			
-					context.write(new Text(fromEdge.toString()),new Text(toEdge.toString()));
+					context.write(new Text(toEdge.toString().split("-")[0]) ,    // a
+								  new Text(fromEdge.toString().split("-")[1]));  // c
 				}
 			}
 
 		}
 	}
+	
+	
+	public static class CompleteTriangleMapper extends Mapper<Object, Text, Text, Text> {
+
+
+		@Override
+		public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
+
+			// Split on comma since input is from a CSV file.
+			final String[] nodes = value.toString().split(",");
+	
+					Text edgeValsTo = new Text(EdgeDirection.To.toString());					
+					context.write(new Text(nodes[1]+","+nodes[0]),edgeValsTo );
+
+				
+		}
+	}
+
+	public static class Path2EdgeMapper extends Mapper<Object, Text, Text, Text> {
+
+
+		@Override
+		public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
+
+			Text edgeValsFrom = new Text(EdgeDirection.From.toString());
+			final String[] nodes = value.toString().split(",");
+			context.write(new Text(nodes[0]+","+ nodes[1]),edgeValsFrom );
+
+		}
+	}
+
+	public static class CompleteTriangleReducer extends Reducer<Text, Text, Text, Text> {
+		private static long count = 0;
+
+		@Override
+		public void reduce(final Text key, final Iterable<Text> values, final Context context) throws IOException, InterruptedException {
+
+			long fromEdges = 0;
+			long toEdges = 0; 
+			for(Text value : values) {
+
+				
+				if(value.toString().equals(EdgeDirection.From.toString())) {
+					fromEdges++;
+				}
+				if(value.toString().equals(EdgeDirection.To.toString())) {
+					toEdges++;
+				}
+
+			}
+			if(toEdges>0) {
+				count = count + fromEdges;
+				context.getCounter(CounterName.Repeated).increment(fromEdges);
+			}
+		}
+		
+		
+
+		@Override
+		public void cleanup(final Context context) throws IOException, InterruptedException{
+
+			long triangles = count/3;
+			Text triangleText = new Text(triangles+"");
+			context.write(new Text("Answer :"),triangleText);
+		}
+	}
+
+	
 
 	@Override
 	public int run(final String[] args) throws Exception {
@@ -102,7 +160,7 @@ public class TriangleCount extends Configured implements Tool {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path("input/edges1.csv"));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path("output"));
 		if(! job.waitForCompletion(true)) {
 			throw new Exception("MR job1 failed");
 		}
@@ -110,20 +168,17 @@ public class TriangleCount extends Configured implements Tool {
 		Job job2 = Job.getInstance(conf,"RSJoin Complete Triangle");
 		job2.setJarByClass(TriangleCount.class);
 	
-//		job2.setReducerClass(CloseTriangleReducer.class);
-//		MultipleInputs.addInputPath(job2, new Path(args[0]), TextInputFormat.class,ThirdEdgeMapper.class);
-//		MultipleInputs.addInputPath(job2, new Path(args[1]), TextInputFormat.class,Path2EdgeMapper.class);
+		job2.setReducerClass(CompleteTriangleReducer.class);
+		MultipleInputs.addInputPath(job2, new Path("input/edges1.csv"), TextInputFormat.class, CompleteTriangleMapper.class);
+		MultipleInputs.addInputPath(job2, new Path("output"), TextInputFormat.class, Path2EdgeMapper.class);
 		//+"/part-r-00000"
 		job2.setOutputKeyClass(Text.class);
 		job2.setOutputValueClass(Text.class);
-		FileOutputFormat.setOutputPath(job2, new Path(args[1]+"-Triangle"));
+		FileOutputFormat.setOutputPath(job2, new Path("output"+"-Triangle"));
 		return job2.waitForCompletion(true) ? 0 : 1;
 	}
 
 	public static void main(final String[] args) {
-		if (args.length != 2) {
-			throw new Error("Two arguments required:\n<input-dir> <output-dir>");
-		}
 		try {
 			ToolRunner.run(new TriangleCount(), args);
 		} catch (final Exception e) {
